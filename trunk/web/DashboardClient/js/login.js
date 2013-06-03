66781a9b18deb5b241b58e32875c8e63ac8e69d2
kettybdo@hotmail.it <kettybdo@hotmail.it@316d50ce-e165-d3dd-9d0b-cda49413bd83>
2013-06-03 15:36:28 +0000

    aggiunta

---
+++ trunk/web/DashboardClient/js/login.js
@@ -0,0 +1,60 @@
+	var p = document.getElementById("pwd-div");
+		var e = document.getElementById("mail-div");
+	    var err = document.getElementById("errore-pwd");
+	    var err_in = document.getElementById("errore-pwd-in");
+	   
+	    
+$("#login").click(function(){
+	 var remember = document.getElementById("remember").checked;
+	eraseCookie("idutente");
+	var username = document.getElementById("username").value;
+	var pwd = hex_md5(document.getElementById("password").value);
+	var url = "http://localhost:8080/DashboardServer/api/Accesso/login/email="+username+"/pwd="+pwd;
+	$.get(url,function(log){
+		if(log == true){
+			 $(err).css("display","none");
+			 $(err_in).css("display","none");
+			 $(e).css("border-color","white");
+			 $(p).css("border-color","white");
+			$.get("http://localhost:8080/DashboardServer/api/Utenti/email/email="+username,function(utente){
+				if(remember){createCookie("idutente",utente.ID,100);}else{createCookie("idutente",utente.ID,0.5);};
+				
+				window.location = "index.html";
+			});//end recupero id utente
+		}
+		else{
+	    
+		$(err).val("Email e/o Password errata!");
+		 $(err_in).css("display","none");
+		 $(err).css("display","inline");
+		 $(p).css("border-color","red");
+		 $(e).css("border-color","red");
+	
+	   
+		};
+		
+	});//end get accesso
+	
+});
+
+
+$("#logout").click(function(){
+	eraseCookie("idutente");
+	eraseCookie("fontecliccata");
+	});
+
+
+
+$("#forgot").click(function(){
+
+
+$("#mail-div").css("display","none");
+$("#pwd-div").css("display","none");
+$("#login").css("display","none");
+$("#remember-id").css("display","none");
+$("#btn-g").css("display","none");
+$("#p").css("display","none");
+var form = document.getElementById("form");
+$(form).css("display","inline");
+
+});//end forgot
